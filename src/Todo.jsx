2543091a40771580654b9b9673a1247a174aec16
martin capovcak <martincapovcak@martins-Mac-Pro.local>
2020-09-29 22:40:55 +0200

    Created todo app

---
+++ src/Todo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Todo = (props) => {
+    //console.log(props);
+    return (
+        <div>
+            <h2>{props.title}</h2>
+        </div>
+    );
+};
+
+export default Todo;
